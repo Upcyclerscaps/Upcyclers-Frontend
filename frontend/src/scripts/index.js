bb3307a092c26aabe0c972b2b0843c9b19da2140
Zaidan Rivandani <74029884+zaidrvnd@users.noreply.github.com>
2024-11-26 12:07:40 +0700

    Update index.js
    mengkonfigurasi index.js

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -5,3 +5,19 @@ import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import App from './views/app';
+import './utils/utils';
+
+const app = new App({
+  content: document.querySelector('#mainContent'),
+  menu: document.querySelector('#navigationDrawer'),
+  drawer: document.querySelector('#hamburgerButton'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
