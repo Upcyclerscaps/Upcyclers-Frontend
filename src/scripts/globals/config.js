dba8546e29fa3c903d5feaa8a1c592f5175fc90d
Zaidan Rivandani <74029884+zaidrvnd@users.noreply.github.com>
2024-12-13 18:07:18 +0700

    update

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const CONFIG = {
-  BASE_URL: 'http://100.25.218.49:5000/api/v1',
-  BASE_IMAGE_URL: 'http://100.25.218.49:5000/images',
+  BASE_URL: 'http://localhost:5000/api/v1',
+  BASE_IMAGE_URL: 'http://localhost:5000/images',
   CACHE_NAME: 'upcyclers-v1',
   DATABASE_NAME: 'upcyclers-database',
   DATABASE_VERSION: 1,
